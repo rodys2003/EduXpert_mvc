183d75e184efa6f98ccfcd75b11c50a3229638d5
rodis2003 <rodisenrique73@gmail.com>
2024-03-11 14:02:00 -0500

    initial project generated by sprint initializr

---
+++ src/main/java/com/rersdev/eduxpert/EduXpertApplication.java
@@ -0,0 +1,13 @@
+package com.rersdev.eduxpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduXpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduXpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rersdev/eduxpert/EduXpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rersdev.eduxpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduXpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
