7755433a771820456a34d7f2621e8ddb13900b3f
rodis2003 <rodisenrique73@gmail.com>
2024-03-14 10:46:04 -0500

    feat: add global exception handler

---
+++ src/main/java/com/rersdev/eduxpert/config/advice/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.rersdev.eduxpert.config.advice.exeption;
+
+import com.rersdev.eduxpert.controller.dto.ApiError;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handlerEntityNotFoundException(EntityNotFoundException exception,
+                                                                 HttpServletRequest request){
+
+        ApiError apiError = ApiError.builder()
+                .message(exception.getMessage())
+                .url(request.getRequestURL().toString())
+                .method(request.getMethod())
+                .timeStamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+}
