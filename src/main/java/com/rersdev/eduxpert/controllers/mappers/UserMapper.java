c55f854f50dba625536c4eff584d9a55b9d37153
rers <rodisenrique73@gmail.com>
2024-08-04 21:40:19 -0500

    feat: add mapper to save a teacher

--- src/main/java/com/rersdev/eduxpert/controllers/mappers/UserMapper.java
+++ src/main/java/com/rersdev/eduxpert/controllers/mappers/UserMapper.java
@@ -1,16 +1,15 @@
 package com.rersdev.eduxpert.controllers.mappers;
 
 import com.rersdev.eduxpert.controllers.dto.get.GetUser;
+import com.rersdev.eduxpert.controllers.dto.insert.NewUser;
 import com.rersdev.eduxpert.persistences.entities.auth.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-
-import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
 
     GetUser toDTO(User user);
 
-    List<GetUser> toDTOList(List<User> users);
+    User toEntity(NewUser newUser);
 }
