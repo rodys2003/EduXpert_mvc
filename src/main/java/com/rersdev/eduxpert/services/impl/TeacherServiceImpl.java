d722b62236424fb7e5b808c1abdf7574d73f7e16
rers <rodisenrique73@gmail.com>
2024-08-04 21:49:17 -0500

    refactor: remove unused saveTeacherMapper

--- src/main/java/com/rersdev/eduxpert/services/impl/TeacherServiceImpl.java
+++ src/main/java/com/rersdev/eduxpert/services/impl/TeacherServiceImpl.java
@@ -3,7 +3,7 @@ package com.rersdev.eduxpert.services.impl;
 import com.rersdev.eduxpert.controllers.dto.get.GetTeacher;
 import com.rersdev.eduxpert.controllers.dto.insert.NewTeacher;
 import com.rersdev.eduxpert.controllers.mappers.TeacherMapper;
-import com.rersdev.eduxpert.controllers.mappers.my_mappers.SaveTeacherMapper;
+import com.rersdev.eduxpert.persistences.entities.enums.TeacherStatus;
 import com.rersdev.eduxpert.persistences.repositories.TeacherRepository;
 import com.rersdev.eduxpert.services.ITeacherService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +12,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -24,21 +23,16 @@ public class TeacherServiceImpl implements ITeacherService {
 
     private final TeacherRepository teacherRepository;
 
-    private final SaveTeacherMapper saveTeacherMapper;
-
-    @Override
-    public void save(NewTeacher teacherDto) {
-        if (teacherDto.getDateStart() == null) {
-            teacherDto.setDateStart(LocalDate.now());
-        }
-        this.teacherRepository.save(saveTeacherMapper.ToEntity(teacherDto));
-    }
-
     private final TeacherMapper teacherMapper;
 
     @Override
+    public void save(NewTeacher teacherDto) {
+        this.teacherRepository.save(teacherMapper.toEntity(teacherDto));
+    }
+
+    @Override
     public Page<GetTeacher> findAll(Pageable pageable) {
-        List<GetTeacher> teacherDtoList = teacherRepository.findAll(pageable).stream()
+        List<GetTeacher> teacherDtoList = teacherRepository.findAllByStatus(TeacherStatus.ACTIVO, pageable).stream()
                 .map(teacherMapper::toGetDTO)
                 .toList();
         return new PageImpl<>(teacherDtoList);
