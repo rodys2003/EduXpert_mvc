982966a0cbb440c4e440262fa4d39c7b51cbf89a
rers <rodisenrique73@gmail.com>
2024-08-09 22:15:20 -0500

    feat: add custom queries

--- src/main/java/com/rersdev/eduxpert/persistences/repositories/TeacherRepository.java
+++ src/main/java/com/rersdev/eduxpert/persistences/repositories/TeacherRepository.java
@@ -5,6 +5,7 @@ import com.rersdev.eduxpert.persistences.entities.enums.TeacherStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -12,5 +13,9 @@ import java.util.UUID;
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
 
-    Page<Teacher> findAllByStatus(TeacherStatus status, Pageable pageable);
+    @Query("SELECT t FROM Teacher t WHERE t.status =?1 AND t.person.user.isActive =?2")
+    Page<Teacher> findAllByStatusAndUserIsActive(TeacherStatus status, boolean isActive, Pageable pageable);
+
+    @Query("SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM Teacher t WHERE t.person.user.documentNumber =?1")
+    boolean existsTeacherByDocumentNumber(String documentNumber);
 }
