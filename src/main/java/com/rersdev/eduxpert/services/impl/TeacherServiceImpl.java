7c0609c0a12456ce339c66a5f84ee3cd02f8d385
rers <rodisenrique73@gmail.com>
2024-08-09 22:13:32 -0500

    feat: add methods to validate status, exist teacher by document

--- src/main/java/com/rersdev/eduxpert/services/impl/TeacherServiceImpl.java
+++ src/main/java/com/rersdev/eduxpert/services/impl/TeacherServiceImpl.java
@@ -6,6 +6,7 @@ import com.rersdev.eduxpert.controllers.dto.users.teacher.TeacherPartialInfoDto;
 import com.rersdev.eduxpert.controllers.dto.users.teacher.TeacherPartialUpdateDto;
 import com.rersdev.eduxpert.controllers.mappers.TeacherMapper;
 import com.rersdev.eduxpert.persistences.entities.Teacher;
+import com.rersdev.eduxpert.persistences.entities.auth.User;
 import com.rersdev.eduxpert.persistences.entities.enums.TeacherStatus;
 import com.rersdev.eduxpert.persistences.repositories.TeacherRepository;
 import com.rersdev.eduxpert.services.ITeacherService;
@@ -29,8 +30,13 @@ public class TeacherServiceImpl implements ITeacherService {
     private final TeacherMapper teacherMapper;
 
     @Override
-    public void save(TeacherDto teacherDto) {
-        this.teacherRepository.save(teacherMapper.toEntity(teacherDto));
+    public UUID save(TeacherDto teacherDto) {
+        String documentNumber = teacherDto.person().user().documentNumber();
+        if (teacherRepository.existsTeacherByDocumentNumber(documentNumber)) {
+            throw new RuntimeException("A user with that document number already exists");
+        }
+        Teacher teacher = this.teacherRepository.save(teacherMapper.toEntity(teacherDto));
+        return teacher.getId();
     }
 
     @Override
@@ -43,8 +49,12 @@ public class TeacherServiceImpl implements ITeacherService {
     }
 
     @Override
-     public Page<TeacherInfoDto> findAll(String status, boolean isActive, Pageable pageable) {
-        List<TeacherInfoDto> teacherDtoList = teacherRepository.findAllByStatusAndUserIsActive(TeacherStatus.valueOf(status), isActive, pageable).stream()
+    public Page<TeacherInfoDto> findAll(String status, boolean isActive, Pageable pageable) {
+        if (status == null) {
+            status = "ACTIVO";
+        }
+        this.validateStatus(status);
+        List<TeacherInfoDto> teacherDtoList = teacherRepository.findAllByStatusAndUserIsActive(TeacherStatus.valueOf(status.toUpperCase()), isActive, pageable).stream()
                 .map(teacherMapper::toDTO)
                 .toList();
         return new PageImpl<>(teacherDtoList);
@@ -56,10 +66,41 @@ public class TeacherServiceImpl implements ITeacherService {
     }
 
     @Override
+    public void updateStatus(String status, UUID id) {
+        this.validateStatus(status);
+
+        Teacher teacherFromDB = teacherRepository.findById(id).orElseThrow(() -> new RuntimeException("Teacher not found"));
+        teacherFromDB.setStatus(TeacherStatus.valueOf(status.toUpperCase()));
+
+        if (status.equalsIgnoreCase("RETIRADO")) {
+            User u = teacherFromDB.getPerson().getUser();
+            u.setIsActive(false);
+            teacherRepository.save(teacherFromDB);
+        }
+
+        teacherRepository.save(teacherFromDB);
+    }
+
+    @Override
     public void deleteByAdmin(UUID id) {
         if (!teacherRepository.existsById(id)) {
             throw new RuntimeException("Teacher not found");
         }
         teacherRepository.deleteById(id);
+    }
+
+    private boolean isValidStatus(String status) {
+        for (TeacherStatus teacherStatus : TeacherStatus.values()) {
+            if (teacherStatus.name().equalsIgnoreCase(status)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void validateStatus(String status) {
+        if (!isValidStatus(status)) {
+            throw new RuntimeException("invalid status: " + status);
+        }
     }
 }
